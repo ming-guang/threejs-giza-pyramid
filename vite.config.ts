24ae91f9c303d9e75b17fa0058d9a5942353f5af
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-12-17 18:17:40 +0700

    fix(vite): adding `TresJS` template compiler options

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      ...templateCompilerOptions,
+    }),
     vueJsx(),
   ],
   resolve: {
